b0bebd5fd8fbd6fe2da0d29bcc44d614446c4911
Bruno <bruno_92k@hotmail.com>
2024-05-03 21:34:32 -0300

    Cenário05 - Em construção

---
+++ Cenario05/src/br/edu/up/modelos/Evento.java
@@ -0,0 +1,11 @@
+package br.edu.up.modelos;
+
+public class Evento {
+    private String nome;
+    private String Local;
+    private String Data;
+    private String LotacaoMaxima;
+    private int IngressosVendidos;
+    private double PrecoIngresso;
+
+}
---
+++ Cenario05/src/br/edu/up/modelos/Reserva.java
@@ -0,0 +1,8 @@
+package br.edu.up.modelos;
+
+public class Reserva {
+    private String NomeResponsavel;
+    private int QuantidadePessoas;
+    private double ValorTotal;
+    private String Data;
+}
