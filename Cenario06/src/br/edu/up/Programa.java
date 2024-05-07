2491b2c7c38822e74e926ea57fb860303091e697
Gabriela <gabibusuda123@gmail.com>
2024-05-06 21:23:38 -0300

    Cenario06 alteraçoes

--- Cenario06/src/br/edu/up/Programa.java
+++ Cenario06/src/br/edu/up/Programa.java
@@ -1,3 +1,5 @@
+//Programa.java
+
 package br.edu.up;
 
 import br.edu.up.controle.*;
@@ -10,37 +12,46 @@ public class Programa {
         ControlePassageiros controlePassageiros = new ControlePassageiros();
 
         ViewPassageiros viewPassageiros = new ViewPassageiros(controlePassageiros);
-        
+
         exibirMenuPrincipal(viewPassageiros);
-        
+
         Prompt.linhaEmBranco();
+
         Prompt.imprimir("Encerrando o programa...");
 
     }
-    
+
     private static void exibirMenuPrincipal(ViewPassageiros viewPassageiros) {
         int opcao;
 
         do {
-        Prompt.separador();
-        Prompt.imprimir("Menu Principal:");
-        Prompt.imprimir("1. Gerenciar Passageiros");
-        Prompt.imprimir("2. Sair");
-        opcao = Prompt.lerInteiro("Escolha uma opção:");
 
-        switch (opcao) {
-            case 1:
-                viewPassageiros.exibirMenu();
-                break;
+            Prompt.separador();
 
-            case 2:
-                Prompt.linhaEmBranco();
-                Prompt.imprimir("Encerrando o programa...");
+            Prompt.imprimir("Menu Principal:");
+            Prompt.imprimir("1. Gerenciar Passageiros");
+            Prompt.imprimir("2. Sair");
 
-                break;
-            default:
-                Prompt.imprimir("Opção inválida. Tente novamente.");
-                
+            opcao = Prompt.lerInteiro("Escolha uma opção:");
+
+            switch (opcao) {
+                case 1:
+
+                    viewPassageiros.exibirMenu();
+
+                    break;
+
+                case 2:
+
+                    Prompt.linhaEmBranco();
+
+                    Prompt.imprimir("Encerrando o programa...");
+
+                    break;
+                default:
+
+                    Prompt.imprimir("Opção inválida. Tente novamente.");
+
             }
         } while (opcao != 2);
     }
--- Cenario06/src/br/edu/up/view/ViewPassageiros.java
+++ Cenario06/src/br/edu/up/view/ViewPassageiros.java
@@ -1,55 +1,101 @@
-// ViewPassageiros.java
+//ViewPassageiros.java
+
 package br.edu.up.view;
 
+import br.edu.up.util.*;
 import br.edu.up.controle.*;
 import br.edu.up.modelo.*;
-import br.edu.up.util.Prompt;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ViewPassageiros {
 
     private ControlePassageiros controlePassageiros;
 
     public ViewPassageiros(ControlePassageiros controlePassageiros) {
+
         this.controlePassageiros = controlePassageiros;
+
     }
 
     public void exibirMenu() {
+
         int opcao;
 
         do {
 
-        Prompt.separador();
+            Prompt.separador();
+            Prompt.imprimir("Menu de Passageiros:");
+            Prompt.imprimir("1. Adicionar Passageiro");
+            Prompt.imprimir("2. Listar Passageiros");
+            Prompt.imprimir("3. Voltar ao Menu Principal");
 
-        Prompt.imprimir("Menu de Passageiros:");
+            opcao = Prompt.lerInteiro("Escolha uma opção:");
 
-        Prompt.imprimir("1. Adicionar Passageiro");
+            switch (opcao) {
+                case 1:
+                    adicionarPassageiro();
+                    break;
+                case 2:
+                    listarPassageiros();
+                    break;
+                case 3:
+                    Prompt.linhaEmBranco();
 
-        Prompt.imprimir("2. Listar Passageiros");
+                    Prompt.imprimir("Voltando ao Menu Principal...");
 
-        Prompt.imprimir("3. Voltar ao Menu Principal");
+                    break;
+                default:
 
-        opcao = Prompt.lerInteiro("Escolha uma opção:");
-        
-        switch (opcao) {
-            case 1:
-                controlePassageiros.adicionarPassageiro();
-                break;
-
-            case 2:
-                listarPassageiros();
-                break;
-
-            case 3:
-                Prompt.linhaEmBranco();
-                Prompt.imprimir("Voltando ao Menu Principal...");
-                break;
-
-            default:
-                Prompt.imprimir("Opção inválida. Tente novamente.");
-
-        }
+                    Prompt.imprimir("Opção inválida. Tente novamente.");
+            }
 
         } while (opcao != 3);
+    }
+
+    private void adicionarPassageiro() {
+
+        if (controlePassageiros.getTamanhoAtual() < controlePassageiros.getTamMax()) {
+
+            Passageiro passageiro = new Passageiro();
+
+            passageiro.setNome(Prompt.lerLinha("Nome do passageiro:"));
+            passageiro.setRg(Prompt.lerLinha("RG do passageiro:"));
+            passageiro.setIdentificadorBagagem(Prompt.lerLinha("Identificador da bagagem:"));
+            passageiro.setPassagem(criarPassagem());
+
+            controlePassageiros.getPassageiros()[controlePassageiros.getTamanhoAtual()] = passageiro;
+
+            controlePassageiros.incrementarTamAtual();
+
+            Prompt.imprimir("Passageiro adicionado com sucesso!");
+
+        } else {
+
+            Prompt.imprimir("Limite máximo de passageiros atingido.");
+
+        }
+    }
+
+    private Passagem criarPassagem() {
+
+        Passagem passagem = new Passagem();
+
+        passagem.setNumeroAssento(Prompt.lerInteiro("Número do assento:"));
+        passagem.setClasseAssento(Prompt.lerLinha("Classe do assento:"));
+        passagem.setDataVoo(lerDataHora("Data e hora do voo:"));
+
+        return passagem;
+    }
+
+    private LocalDateTime lerDataHora(String mensagem) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        String dataHoraString = Prompt.lerLinha(mensagem + " (no formato dd/MM/yyyy HH:mm):");
+
+        return LocalDateTime.parse(dataHoraString, formatter);
     }
 
     private void listarPassageiros() {
@@ -65,6 +111,7 @@ public class ViewPassageiros {
             if (passageiro != null) {
 
                 Prompt.imprimir(passageiro);
+
             }
         }
     }
